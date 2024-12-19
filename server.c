edf7bb41b214ff1565f733c23615438a48e291d9
ist199417 <ist199417@tecnico.ulisboa.pt>
2024-12-19 15:20:53 +0000

    divisao

--- server.c
+++ server.c
@@ -17,10 +17,10 @@
 #define STR 4
 #define SSB 5
 
-int fd, errcode;
+int fd_udp, errcode;
 ssize_t n;
 socklen_t addrlen;
-struct addrinfo hints, *res;
+struct addrinfo hints_udp, *res_udp;
 struct sockaddr_in addr;
 
 char get_color(const char *str) {
@@ -28,7 +28,7 @@ char get_color(const char *str) {
     if (!str || str[1] != '\0') {
         return '\0'; // Return '\0' if the string is invalid or not size 1
     }
-    
+
     char c = str[0]; // Extract the single character
     const char valid_chars[] = {'R', 'G', 'B', 'Y', 'O', 'P'};
     int size = sizeof(valid_chars) / sizeof(valid_chars[0]);
@@ -215,6 +215,50 @@ void print_games(const GameArray *array) {
     }
 }
 
+int is_correct_guess(GameArray *array, int PLID, char iC1, char iC2, char iC3, char iC4) {
+    int game_index = get_game_index(PLID, array);
+    char key[5];
+    sprintf(key, "%s", array -> games[game_index].key);
+    return key[0] == iC1 && key[1] == iC2 && key[2] == iC3 && key[3] == iC4;
+}
+
+int is_repeated_try(GameArray *array, int PLID, char iC1, char iC2, char iC3, char iC4) {
+
+}
+
+int is_resend(GameArray *array, int PLID, int nT) {
+
+}
+
+void calculateBlackAndWhite(const char* secret, const char* guess, int* nB, int* nW) {
+    int length = strlen(secret);
+    int black = 0, white = 0;
+
+    // Arrays to track counts of unmatched characters
+    int secretCount[256] = {0};
+    int guessCount[256] = {0};
+
+    // First pass: count black matches
+    for (int i = 0; i < length; i++) {
+        if (secret[i] == guess[i]) {
+            black++;
+        } else {
+            // Track unmatched characters
+            secretCount[(unsigned char)secret[i]]++;
+            guessCount[(unsigned char)guess[i]]++;
+        }
+    }
+
+    // Second pass: count white matches
+    for (int i = 0; i < 256; i++) {
+        white += (secretCount[i] < guessCount[i]) ? secretCount[i] : guessCount[i];
+    }
+
+    // Assign the results
+    *nB = black;
+    *nW = white;
+}
+
 int main(int argc, char *argv[]) {
     char* GSport = "58080";
     int verbose = 0;
@@ -234,21 +278,21 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd == -1) {
+    fd_udp = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd_udp == -1) {
         exit(1);
     }
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
+    memset(&hints_udp, 0, sizeof hints_udp);
+    hints_udp.ai_family = AF_INET;
+    hints_udp.ai_socktype = SOCK_DGRAM;
     /* É passada uma flag para indicar que o socket é passivo.
     Esta flag é usada mais tarde pela função `bind()` e indica que
     o socket aceita conceções. */
-    hints.ai_flags = AI_PASSIVE;
+    hints_udp.ai_flags = AI_PASSIVE;
 
     /* Ao passar o endereço `NULL`, indicamos que somos nós o Host. */
-    errcode = getaddrinfo(NULL, GSport, &hints, &res);
+    errcode = getaddrinfo(NULL, GSport, &hints_udp, &res_udp);
     if (errcode != 0) {
         exit(1);
     }
@@ -257,13 +301,13 @@ int main(int argc, char *argv[]) {
     Esta função serve para associar um endereço à socket, de forma a ser acessível
     por conexões externas ao programa.
     É associado o nosso endereço (`res->ai_addr`, definido na chamada à função `getaddrinfo()`).*/
-    n = bind(fd, res->ai_addr, res->ai_addrlen);
+    n = bind(fd_udp, res_udp->ai_addr, res_udp->ai_addrlen);
     if (n == -1) {
         exit(1);
     }
 
     int max_size = 512;
-    char buffer[max_size], PLID[max_size], status[4];
+    char buffer[max_size], PLID[max_size], status[max_size];
     GameArray games_array;
     init_game(&games_array);
 
@@ -273,7 +317,7 @@ int main(int argc, char *argv[]) {
         /* Lê da socket (fd) 128 bytes e guarda-os no buffer.
         Existem flags opcionais que não são passadas (0).
         O endereço do cliente (e o seu tamanho) são guardados para mais tarde devolver o texto */
-        n = recvfrom(fd, buffer, 128, 0, (struct sockaddr *)&addr, &addrlen);
+        n = recvfrom(fd_udp, buffer, 128, 0, (struct sockaddr *)&addr, &addrlen);
         if (n == -1) {
             exit(1);
         }
@@ -327,11 +371,30 @@ int main(int argc, char *argv[]) {
                 int iPLID = get_integer(PLID, 6), inT = get_integer(nT, 1), game_index = get_game_index(iPLID, &games_array);
                 char iC1 = get_color(C1), iC2 = get_color(C2), iC3 = get_color(C3), iC4 = get_color(C4);
                 printf("Try: %d, %d, %c, %c, %c, %c\n", iPLID, inT, iC1, iC2, iC3, iC4);
+                printf("Correct guess? %d\n", is_correct_guess(&games_array, iPLID, iC1, iC2, iC3, iC4));
                 if (iPLID == -1 || inT == -1 || iC1 == '\0' || iC2 == '\0' || iC3 == '\0' || iC4 == '\0') {
-                    sprintf(status, "%s", "ERR");
+                    sprintf(status, "ERR");
                 } else if (has_x_seconds_passed(games_array.games[game_index].start_time, games_array.games[game_index].max_playtime)) {
+                    char key[5];
+                    sprintf(key, "%s", games_array.games[game_index].key);
                     remove_game(&games_array, game_index);
-                    sprintf(status, "%s", "ETM");
+                    sprintf(status, "ETM %c %c %c %c", key[0], key[1], key[2], key[3]);
+                } else if (inT == 8 && !is_correct_guess(&games_array, iPLID, iC1, iC2, iC3, iC4)) {
+                    char key[5];
+                    sprintf(key, "%s", games_array.games[game_index].key);
+                    remove_game(&games_array, game_index);
+                    sprintf(status, "ENT %c %c %c %c", key[0], key[1], key[2], key[3]);
+                } else if (is_repeated_try(&games_array, iPLID, iC1, iC2, iC3, iC4) && !is_resend(&games_array, iPLID, inT)) {
+                    sprintf(status, "DUP");
+                } else {
+                    char key[5];
+                    sprintf(key, "%s", games_array.games[game_index].key);
+                    char try[5];
+                    sprintf(try, "%c%c%c%c", iC1, iC2, iC3, iC4);
+                    int nB = 0, nW = 0;
+                    calculateBlackAndWhite(key, try, &nB, &nW);
+                    add_try(&games_array.games[game_index], try);
+                    sprintf(status, "OK %d %d %d", inT, nB, nW);
                 }
                 // A PARTIR DAQUI NAO ESTA FEITO
                 sprintf(buffer, "RTR %s\n", status);
@@ -342,13 +405,13 @@ int main(int argc, char *argv[]) {
         printf("message sent: %s\n", buffer);
         print_games(&games_array);
         /* Envia a mensagem recebida (atualmente presente no buffer) para o endereço `addr` de onde foram recebidos dados */
-        n = sendto(fd, buffer, strlen(buffer), 0, (struct sockaddr *)&addr, addrlen);
+        n = sendto(fd_udp, buffer, strlen(buffer), 0, (struct sockaddr *)&addr, addrlen);
         if (n == -1) {
             exit(1);
         }
     }
 
     free_game_array(&games_array);
-    freeaddrinfo(res);
-    close(fd);
+    freeaddrinfo(res_udp);
+    close(fd_udp);
 }
